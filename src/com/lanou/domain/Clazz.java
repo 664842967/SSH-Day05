5f90ca3d544e0d636d734d518577e2b8494df9f1
664842967 <664842967@qq.com>
2018-01-08 17:59:05 +0800

    初始化,提交代码提交

---
+++ src/com/lanou/domain/Clazz.java
@@ -0,0 +1,54 @@
+package com.lanou.domain;
+
+import java.io.Serializable;
+
+/**
+ * Created by dllo on 18/1/8.
+ */
+public class Clazz implements Serializable{
+    private int cid;
+    private String cname;
+    private String cinfor; // 班级信息
+
+
+    public Clazz() {
+    }
+
+    public Clazz(String cname, String cinfor) {
+        this.cname = cname;
+        this.cinfor = cinfor;
+    }
+
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    public String getCname() {
+        return cname;
+    }
+
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+
+    public String getCinfor() {
+        return cinfor;
+    }
+
+    public void setCinfor(String cinfor) {
+        this.cinfor = cinfor;
+    }
+
+    @Override
+    public String toString() {
+        return "Clazz{" +
+                "cid=" + cid +
+                ", cname='" + cname + '\'' +
+                ", cinfor='" + cinfor + '\'' +
+                '}';
+    }
+}
